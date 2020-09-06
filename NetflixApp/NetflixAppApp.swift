a29be5bc4cda9cbeb3dfce4d75e33066ea39ba03
wanhar <wanhar@sehatq.com>
2020-09-06 18:38:47 +0700

    Initial Commit

---
+++ NetflixApp/NetflixAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixAppApp.swift
+//  NetflixApp
+//
+//  Created by Wanhar on 06/09/20.
+//
+
+import SwiftUI
+
+@main
+struct NetflixAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
